83423be23e5f6c0b38e95551db323fd52d51dee1
gabsn <gabin.marignier@datadoghq.com>
2017-07-27 15:16:05 -0400

    wip

--- client/main.go
+++ client/main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
 	"sync"
-	"sync/atomic"
+	"strings"
+	"errors"
+
+	"github.com/DataDog/dd-trace-go/tracer"
 )
 
 var (
-	wg                   = &sync.WaitGroup{}
-	currentRequest int64 = 0
-	parallelConn         = 1000
-	addr                 = "http://localhost:8080"
+	wg           = &sync.WaitGroup{}
+	parallelConn = 1000
+	addr         = "http://localhost:8080"
 )
 
 func main() {
@@ -28,20 +25,12 @@ func main() {
 func requestRoutine() {
 	defer wg.Done()
 	for {
-		curr := atomic.AddInt64(&currentRequest, 1)
-		doRequest(curr)
+		doRequest()
 	}
 }
 
-func doRequest(i int64) {
-	resp, err := http.Get(addr)
-	if err != nil {
-		fmt.Printf("error on request: %d (%s)\n", i, err)
-		os.Exit(1)
-	}
-	if _, err = io.Copy(ioutil.Discard, resp.Body); err != nil {
-		fmt.Printf("error on body read: %d (%s)\n", i, err)
-		os.Exit(1)
-	}
-	resp.Body.Close()
+func doRequest() {
+	span := tracer.NewRootSpan("test-go", "panic", "go")
+	span.SetError(errors.New(strings.Repeat("a", 10)))
+	span.Finish()
 }
--- server/main.go
+++ server/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello")
+		w.WriteHeader(104)
 	}))
 }
